c884fb45ca59c3c4940663305156a7b2c9f97e10
Won Ying Keat <yingkeatwon@gmail.com>
2020-12-17 16:37:34 +0800

    Cleaned up the script Relative files issues solved

--- setup.sh
+++ setup.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+# This script performs the basic set-up to ensure everything runs well and good
+# Ensure you have ZOAU installed in USS: https://www.ibm.com/support/knowledgecenter/en/SSKFYE_1.0.1/install.html
+# We hope
+
 echo "Checking required files"
 file="requirements.txt"
+script_dir=$(dirname "$BASH_SOURCE")
+current_dir=$(pwd)
+if [ $script_dir = '.' ]; then
+  script_dir="$current_dir"
+fi
 # Check if requirements.txt exists
-if [[ -e "$file" ]] && [[ -s "$file" ]]; then
+if [[ -e "$script_dir/$file" ]] && [[ -s "$script_dir/$file" ]]; then
   echo "Downloading pip modules"
-  pip3 install -r "$file"
+  pip3 install -r "$script_dir/$file"
+
+  # Make sure they are executable
+  chmod +x "$script_dir/start.py" "$script_dir/jobStat.py"
 
   # Add banner to whatever is the choice of the user
   echo "Add start-up banner? [y/N]"
@@ -15,15 +27,7 @@ if [[ -e "$file" ]] && [[ -s "$file" ]]; then
   if [[ "$addBool" =~ ^y(es)?$ ]]; then
     echo "Add to Z/OS Shell (z) or bash (b)?"
     read -n 1 shell
-    # current_path=$(readlink -f "$0")
-    script_dir=$(dirname "$BASH_SOURCE")
-    current_dir=$(pwd)
-    if [ $script_dir = '.' ]
-    then
-    script_dir="$current_dir"
-    fi
-    echo $script_dir
-    # current_dir=$(dirname "$current_path")
+
 
     if [[ "$shell" == z ]]; then
       echo "Adding banner to Z/OS shell"
@@ -36,7 +40,8 @@ if [[ -e "$file" ]] && [[ -s "$file" ]]; then
     fi
   fi
 
-  echo "Set up is complete."
+  printf "Set up is complete.\n\n"
+  printf "Customise your start-up banner in settings.py\n\n"
   echo "Find out more about jobStat using jobStat.py --help"
 else
   echo "Stopping... Some files are missing."
